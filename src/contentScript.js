93e2f4477216cc195d9c92c764a718e4a186e0cb
path-yu <1974675011@qq.com>
2023-05-22 23:29:35 +0800

    update

--- src/contentScript.js
+++ src/contentScript.js
@@ -10,18 +10,63 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 let timer;
 let containerRoot;
-timer = setInterval(() => {
-  containerRoot = document.querySelector("#opportunities");
-  if (containerRoot != null) {
-    clearInterval(timer);
-    init();
+
+const checkLocationPathName = () => {
+  if (location.pathname.includes("surveys:history")) {
+    timer = setInterval(() => {
+      containerRoot = document.querySelector("#opportunities");
+      if (containerRoot != null) {
+        init();
+        changeMainStyle();
+        listenerRootMain();
+        clearInterval(timer);
+      }
+    }, 300);
+  } else {
+    timer = setInterval(() => {
+      if (
+        document.querySelector("app-reward") ||
+        document.querySelector("app-profiling") ||
+        document.querySelector("#app-root > main > app-survey")
+      ) {
+        changeMainStyle();
+        listenerRootMain();
+        clearInterval(timer);
+      }
+    }, 300);
   }
-}, 1000);
+};
+console.log("render");
+if (location.host == "panelist.cint.com") {
+  if (location.pathname == "/") {
+    let cancanId = setInterval(() => {
+      if (location.pathname != "/") {
+        clearInterval(cancanId);
+        setTimeout(checkLocationPathName);
+      }
+    }, 200);
+  } else {
+    checkLocationPathName();
+  }
+}
+
+const changeMainStyle = () => {
+  document.querySelector(
+    `#app-root > main > app-${
+      location.pathname.includes("account:settings")
+        ? "profiling"
+        : location.pathname.includes("surveys:history")
+        ? "survey"
+        : "reward"
+    } > section`
+  ).style.marginTop = "61px";
+};
 let hasOwnChangeChildList = false;
 let observer = null;
 const init = () => {
   // 创建一个新的MutationObserver对象
   observer = new MutationObserver((mutationsList, observer) => {
+    console.log("childList");
     // 在DOM节点改变时执行的回调函数
     for (const mutation of mutationsList) {
       if (mutation.type == "childList" && !hasOwnChangeChildList) {
@@ -47,64 +92,74 @@ const changeList = () => {
 
   for (let index = 0; index < children.length; index++) {
     const child = children[index];
+    let oppId = child.getAttribute("data-opp-id");
+    let tableELe = child.querySelector(".table > .table-cell");
+    // bind id
+    const button = child.querySelector(".take-survey");
+    button.setAttribute("data-opp-id", oppId);
+    button.removeEventListener("click", handleClick);
+    button.addEventListener("click", handleClick);
 
-    if (child.nodeType == 1) {
-      let oppId = child.getAttribute("data-opp-id");
-      let tableELe = child.querySelector(".table > .table-cell");
-      // bind id
-      const button = child.querySelector(".take-survey");
-      button.setAttribute("data-opp-id", oppId);
-      button.removeEventListener("click", handleClick);
-      button.addEventListener("click", handleClick);
+    if (
+      hasOpenSelectValue == "all" &&
+      statusSelectValue == "all" &&
+      moneySelectValue == "all"
+    ) {
+      child.style.display = "block";
+    } else {
+      let data = questionMap[oppId];
+      if (data) {
+        changeChildDisplay(child, data);
+      }
+    }
 
-      if (!tableELe.textContent.includes("问卷编号：")) {
-        let idText = document.createElement("div");
-        idText.textContent = `问卷编号：${oppId}`;
-        idText.style.cssText = "color:red; padding-left:10px;";
+    if (!tableELe.textContent.includes("问卷编号：")) {
+      let idText = document.createElement("div");
+      idText.textContent = `问卷编号：${oppId}`;
+      idText.style.cssText = "color:red; padding-left:10px;";
 
-        let fragment = document.createDocumentFragment();
-        let hasOpenEle = document.createElement("span");
-        hasOpenEle.setAttribute("class", "hasOpen");
-        hasOpenEle.style.paddingLeft = "10px";
+      let fragment = document.createDocumentFragment();
+      let hasOpenEle = document.createElement("span");
+      hasOpenEle.setAttribute("class", "hasOpen");
+      hasOpenEle.style.paddingLeft = "10px";
 
-        let otherEle = document.createElement("span");
-        otherEle.setAttribute("class", "test_status");
-        otherEle.style.color = "black";
-        fragment.appendChild(idText);
-        fragment.appendChild(hasOpenEle);
-        fragment.appendChild(otherEle);
-        const childrenAsArray = Array.from(fragment.children);
-        for (const element of childrenAsArray) {
-          tableELe.insertAdjacentElement("afterbegin", element);
-        }
-        if (sessionStorage.getItem(oppId)) {
+      let otherEle = document.createElement("span");
+      otherEle.setAttribute("class", "test_status");
+      otherEle.style.color = "black";
+      fragment.appendChild(idText);
+      fragment.appendChild(hasOpenEle);
+      fragment.appendChild(otherEle);
+      const childrenAsArray = Array.from(fragment.children);
+      for (const element of childrenAsArray) {
+        tableELe.insertAdjacentElement("afterbegin", element);
+      }
+      if (sessionStorage.getItem(oppId)) {
+        questionMap[oppId] = {
+          ...JSON.parse(sessionStorage.getItem(oppId)),
+          hasOpenEle,
+          otherEle,
+        };
+        renderDom(questionMap[oppId], hasOpenEle, otherEle);
+      } else {
+        if (!questionMap[oppId]) {
           questionMap[oppId] = {
-            ...JSON.parse(sessionStorage.getItem(oppId)),
+            hasOpen: false,
+            id: oppId,
+            status: 0, // 0 未知， 1 -> 可做， 2 -> 不合适,
             hasOpenEle,
             otherEle,
+            tabsId: null,
+            money: child
+              .querySelector(".opportunity-incentive")
+              .textContent.trim(),
           };
-          renderDom(questionMap[oppId], hasOpenEle, otherEle);
+          hasOwnChangeChildList = true;
+          hasOpenEle.textContent = "未打开";
+          hasOpenEle.style.color = "#FF69B4";
+          otherEle.textContent = `问卷状态：未知`;
+          setTimeout(() => (hasOwnChangeChildList = false));
         } else {
-          if (!questionMap[oppId]) {
-            questionMap[oppId] = {
-              hasOpen: false,
-              id: oppId,
-              status: 0, // 0 未知， 1 -> 可做， 2 -> 不合适,
-              hasOpenEle,
-              otherEle,
-              tabsId: null,
-              money: child
-                .querySelector(".opportunity-incentive")
-                .textContent.trim(),
-            };
-            hasOwnChangeChildList = true;
-            hasOpenEle.textContent = "未打开";
-            hasOpenEle.style.color = "#FF69B4";
-            otherEle.textContent = `问卷状态：未知`;
-            setTimeout(() => (hasOwnChangeChildList = false));
-          } else {
-            renderDom(questionMap[oppId], hasOpenEle, otherEle);
-          }
+          renderDom(questionMap[oppId], hasOpenEle, otherEle);
         }
       }
     }
@@ -180,66 +235,147 @@ if (location.host == "panelist.cint.com") {
     if (message.type == "refreshList") {
       containerRoot = document.querySelector("#opportunities");
       if (containerRoot != null && location.pathname.includes("surveys")) {
+        console.log("refresh");
         observer.disconnect();
         init();
         changeList();
-      }
-    }
-
-    if (
-      message.type == "selectOpenChange" ||
-      message.type == "statusChange" ||
-      message.type == "moneyChange"
-    ) {
-      hasOwnChangeChildList = true;
-      if (message.type == "selectOpenChange") {
-        hasOpenSelectValue = message.selectValue;
-      }
-      if (message.type == "statusChange") {
-        statusSelectValue = message.selectValue;
-      }
-      if (message.type == "moneyChange") {
-        moneySelectValue = message.selectValue;
-      }
-      hasOwnChangeChildList = true;
-
-      if (
-        hasOpenSelectValue == "all" &&
-        statusSelectValue == "all" &&
-        moneySelectValue == "all"
-      ) {
-        console.log();
-        for (let index = 0; index < containerRoot.children.length; index++) {
-          const child = containerRoot.children[index];
-          child.style.display = "block";
-        }
-        setTimeout(() => (hasOwnChangeChildList = false));
-      } else {
-        Array.from(containerRoot.children).forEach((child) => {});
-        for (let index = 0; index < containerRoot.children.length; index++) {
-          const child = containerRoot.children[index];
-          let oppId = child.getAttribute("data-opp-id");
-          let data = questionMap[oppId];
-          let money = parseFloat(data["money"]);
-          let firstValue =
-            hasOpenSelectValue == "all"
-              ? true
-              : data["hasOpen"] + "" == hasOpenSelectValue;
-          let secondValue =
-            statusSelectValue == "all"
-              ? true
-              : data["status"] == statusSelectValue;
-          let thirdValue =
-            moneySelectValue == "all"
-              ? true
-              : validationMoneyMap[moneySelectValue](money);
-          child.style.display =
-            firstValue && secondValue && thirdValue ? "block" : "none";
-        }
-        setTimeout(() => (hasOwnChangeChildList = false));
       }
     }
     sendResponse({});
     return true;
   });
 }
+
+if (location.host == "panelist.cint.com") {
+  const html = `
+      <div class="flex mt10" style="
+      background: linear-gradient(rgb(252, 199, 12) 0%, rgb(218, 172, 11) 100%);
+      width: 100%;
+      justify-content: center;
+      display: flex;
+      position: fixed;
+      top: 61px;
+      z-index: 999;
+      padding:10px;
+      ">
+        <div class="selectContainer">
+          <label for="open-select">是否打开：</label>
+          <select id="open-select">
+            <option value="all">全部</option>
+            <option value="true">是</option>
+            <option value="false">否</option>
+          </select>
+        </div>
+        <div class="selectContainer " style="margin-left:20px">
+          <label for="status-select">问卷状态：</label>
+          <select id="status-select" class="flex items-center">
+            <option value="all">全部</option>
+            <option value="0">未知</option>
+            <option value="1">测试成功</option>
+            <option value="2">不合适</option>
+          </select>
+        </div>
+        <div class="selectContainer" style="margin-left:20px">
+          <label for="money-select">价值区间筛选：</label>
+          <select id="money-select" class="flex items-center">
+            <option value="all">全部</option>
+            <option value="1">0.5~1区间</option>
+            <option value="2">1~2区间</option>
+            <option value="3">2~3区间</option>
+            <option value="4">3元以上</option>
+          </select>
+        </div>
+      </div>
+  `;
+  document.body.insertAdjacentHTML("afterbegin", html);
+  setTimeout(() => {
+    const openSelect = document.getElementById("open-select");
+    const statusSelect = document.getElementById("status-select");
+    const moneySelect = document.getElementById("money-select");
+    openSelect.addEventListener("change", async (event) => {
+      hasOpenSelectValue = event.target.value;
+      changeChildrenDisplay();
+    });
+    statusSelect.addEventListener("change", async (event) => {
+      statusSelectValue = event.target.value;
+      changeChildDisplay();
+    });
+    moneySelect.addEventListener("change", async (event) => {
+      moneySelectValue = event.target.value;
+      changeChildrenDisplay();
+    });
+  });
+  console.log("hello world");
+}
+let toolbarObserver;
+const listenerRootMain = () => {
+  // 创建一个新的MutationObserver对象
+  toolbarObserver = new MutationObserver(() => {
+    // 在DOM节点改变时执行的回调函数
+    changeMainStyle();
+    if (
+      location.pathname.includes("account:settings") ||
+      (location.pathname.includes("reward") && observer != null)
+    ) {
+      observer.disconnect();
+      observer = null;
+    }
+    if (location.pathname.includes("surveys:history") && observer == null) {
+      listenOpportunitiesEleChange();
+    }
+  });
+
+  // 配置MutationObserver对象以监视子节点列表的变化
+  const config = { childList: true };
+
+  // 开始监视DOM节点
+  toolbarObserver.observe(document.querySelector("#app-root > main"), config);
+};
+const listenOpportunitiesEleChange = () => {
+  timer = setInterval(() => {
+    containerRoot = document.querySelector("#opportunities");
+    if (containerRoot != null) {
+      init();
+      clearInterval(timer);
+    }
+  }, 300);
+};
+const changeChildDisplay = (child, data) => {
+  let money = parseFloat(data["money"]);
+  let firstValue =
+    hasOpenSelectValue == "all"
+      ? true
+      : data["hasOpen"] + "" == hasOpenSelectValue;
+  let secondValue =
+    statusSelectValue == "all" ? true : data["status"] == statusSelectValue;
+  let thirdValue =
+    moneySelectValue == "all"
+      ? true
+      : validationMoneyMap[moneySelectValue](money);
+  child.style.display =
+    firstValue && secondValue && thirdValue ? "block" : "none";
+};
+const changeChildrenDisplay = () => {
+  hasOwnChangeChildList = true;
+  if (
+    hasOpenSelectValue == "all" &&
+    statusSelectValue == "all" &&
+    moneySelectValue == "all"
+  ) {
+    for (let index = 0; index < containerRoot.children.length; index++) {
+      const child = containerRoot.children[index];
+      child.style.display = "block";
+    }
+    setTimeout(() => (hasOwnChangeChildList = false));
+  } else {
+    for (let index = 0; index < containerRoot.children.length; index++) {
+      const child = containerRoot.children[index];
+      let oppId = child.getAttribute("data-opp-id");
+      let data = questionMap[oppId];
+      if (data) {
+        changeChildDisplay(child, data);
+      }
+    }
+    setTimeout(() => (hasOwnChangeChildList = false));
+  }
+};
--- src/popup.js
+++ src/popup.js
@@ -16,20 +16,6 @@ import "./popup.css";
           response.value.money;
       }
     });
-
-    let statusSelectValue = localStorage.getItem("statusSelectValue");
-    let selectOpenValue = localStorage.getItem("selectOpenValue");
-    let moneySelectValue = localStorage.getItem("moneySelectValue");
-
-    if (statusSelectValue) {
-      statusSelect.value = statusSelectValue;
-    }
-    if (selectOpenValue) {
-      openSelect.value = selectOpenValue;
-    }
-    if (moneySelectValue) {
-      moneySelect.value = moneySelectValue;
-    }
   }
 
   document.addEventListener("DOMContentLoaded", init);
@@ -38,15 +24,14 @@ import "./popup.css";
   const disabledButton = document.querySelector(".disabledButton");
   const availableButton = document.querySelector(".availableButton");
   const refreshButton = document.querySelector(".refreshButton");
-  const openSelect = document.querySelector("#open-select");
-  const statusSelect = document.querySelector("#status-select");
-  const moneySelect = document.querySelector("#money-select");
   const oppOrderButton = document.querySelector(".oppOrderButton");
 
   openButton.addEventListener("click", async () => {
     const result = await chrome.storage.session.get(["iframeUrl"]);
     if (result.iframeUrl) {
       window.open(result.iframeUrl);
+    } else {
+      renderMessage(1, "页面未初始化完成!");
     }
   });
 
@@ -71,65 +56,30 @@ import "./popup.css";
     }
   });
 
-  refreshButton.addEventListener("click", async (ev) => {
-    if (result["rootTabId"]) {
-      chrome.tabs.sendMessage(+activeTabId, {
-        type: "refreshList",
-      });
-    }
-  });
-  openSelect.addEventListener("change", async (event) => {
-    let result = await chrome.storage.session.get("rootTabId");
-    if (result["rootTabId"]) {
-      chrome.tabs.sendMessage(+result["rootTabId"], {
-        type: "selectOpenChange",
-        selectValue: event.target.value,
-      });
-      localStorage.setItem("selectOpenValue", event.target.value);
-    }
-  });
-  statusSelect.addEventListener("change", async (event) => {
-    let result = await chrome.storage.session.get("rootTabId");
-    if (result["rootTabId"]) {
-      chrome.tabs.sendMessage(+result["rootTabId"], {
-        type: "statusChange",
-        selectValue: event.target.value,
-      });
-      localStorage.setItem("statusSelectValue", event.target.value);
-    }
-  });
-  moneySelect.addEventListener("change", async (event) => {
-    console.log("change");
-    let result = await chrome.storage.session.get("rootTabId");
-    if (result["rootTabId"]) {
-      chrome.tabs.sendMessage(+result["rootTabId"], {
-        type: "moneyChange",
-        selectValue: event.target.value,
-      });
-      localStorage.setItem("moneySelectValue", event.target.value);
-    }
-  });
   oppOrderButton.addEventListener("click", (ev) => {
     if (oppOrderButton.textContent != "未知") {
       navigator.clipboard.writeText(oppOrderButton.textContent);
-      const message = "复制成功！";
-      const div = document.createElement("div");
-      div.id = "copy-message";
-      div.textContent = message;
-      document.body.appendChild(div);
-
-      // 在下一帧中切换提示元素的可见状态，触发过渡效果
-      window.requestAnimationFrame(() => {
-        div.classList.add("visible");
-      });
-
-      // 3秒后删除提示元素
-      setTimeout(() => {
-        div.classList.remove("visible");
-        setTimeout(() => {
-          document.body.removeChild(div);
-        }, 300); // 等待0.3秒，使过渡效果结束后再删除元素
-      }, 3000);
+      renderMessage();
     }
   });
+
+  const renderMessage = (type = 0, message = "复制成功") => {
+    const div = document.createElement("div");
+    div.id = type == 0 ? "copy-message" : "error-message";
+    div.textContent = message;
+    document.body.appendChild(div);
+
+    // 在下一帧中切换提示元素的可见状态，触发过渡效果
+    window.requestAnimationFrame(() => {
+      div.classList.add("visible");
+    });
+
+    // 3秒后删除提示元素
+    setTimeout(() => {
+      div.classList.remove("visible");
+      setTimeout(() => {
+        document.body.removeChild(div);
+      }, 300); // 等待0.3秒，使过渡效果结束后再删除元素
+    }, 3000);
+  };
 })();
